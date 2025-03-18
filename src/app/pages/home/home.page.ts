ba93d8b891315a43a6ce2bff1e2209540c3920a1
Fabian Kargl <mt221024@fhstp.ac.at>
2025-03-18 14:59:19 +0100

    added quiz

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,27 +1,41 @@
 import { Component, inject } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from '@ionic/angular/standalone';
 import { DataService } from 'src/app/services/data.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+  ],
 })
 export class HomePage {
-
   public data = inject(DataService);
   private router = inject(Router);
 
-  constructor() {
-    
-  }
+  constructor() {}
   showList() {
     this.router.navigate(['/question-list']);
   }
+
+  startQuiz = () => {
+    this.router.navigate(['/quiz']);
+  };
 }
--- src/app/pages/quiz/quiz.page.ts
+++ src/app/pages/quiz/quiz.page.ts
@@ -1,20 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonButton,
+  ToastController,
+  IonModal,
+} from '@ionic/angular/standalone';
+import { Question } from 'src/app/services/Question';
+import { DataService } from 'src/app/services/data.service';
+import { Router } from '@angular/router';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.page.html',
   styleUrls: ['./quiz.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonModal,
+    IonButton,
+    IonList,
+    IonLabel,
+    IonItem,
+    IonButtons,
+    IonBackButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class QuizPage implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
 
-  constructor() { }
+  public data = inject(DataService);
+  public toastController = inject(ToastController);
+
+  public shuffledQuestions: Question[] = [];
+  public pointer: number = 0;
+  public currElement: Question | null = null;
+  public score: number = 0;
+
+  public toastMessage: string = '';
+  public toastColor: string = '';
+
+  private router = inject(Router);
+
+  constructor() {}
 
   ngOnInit() {
+    this.shuffledQuestions = [...this.data.currentQuiz.questions];
+    this.shuffleArray(this.shuffledQuestions);
+    this.pointer = 0;
+    this.currElement = this.shuffledQuestions[this.pointer];
+    console.log(this.shuffledQuestions);
   }
 
+  private shuffleArray(array: Question[]): void {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  async checkAnswer(selectedAnswer: number) {
+    const isCorrect =
+      this.currElement && selectedAnswer === this.currElement.correct;
+
+    this.toastMessage = isCorrect ? 'Correct!' : 'Wrong!';
+    this.toastColor = isCorrect ? 'success' : 'danger';
+
+    await this.showToast();
+
+    if (isCorrect) {
+      this.score++;
+    }
+    this.pointer++;
+    if (this.pointer < this.shuffledQuestions.length) {
+      this.currElement = this.shuffledQuestions[this.pointer];
+    } else {
+      this.modal.present();
+    }
+  }
+
+  public async showToast() {
+    const toast = await this.toastController.create({
+      message: this.toastMessage,
+      duration: 1000,
+      color: this.toastColor,
+    });
+    await toast.present();
+  }
+
+  public confirm() {
+    this.modal.dismiss(null, 'confirm');
+    this.router.navigate(['/home']);
+  }
 }
